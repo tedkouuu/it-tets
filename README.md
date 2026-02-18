812a3240d7f110602c8f8e1d5a45995597339375
Teodor <teodor.orlinov.trendafilov@gmail.com>
2026-02-18 14:37:05 +0200

    eg2

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-﻿IT project
+﻿GuardNOC website
--- src/config/brand.ts
+++ src/config/brand.ts
@@ -1,11 +1,11 @@
-export const brandName = "IT Managed Services";
-export const legalName = "IT Managed Services EOOD";
-export const domain = "example.com";
-export const siteUrl = "https://example.com";
+export const brandName = "GuardNOC";
+export const legalName = "GuardNOC EOOD";
+export const domain = "guardnoc.com";
+export const siteUrl = "https://guardnoc.com";
 export const phone = "0888271714";
 export const defaultOGImage = "/og.jpg";
 export const shortDescription =
-  "Професионални управлявани IT услуги за фирми в България - поддръжка, мрежи, облак, киберсигурност и надежден 24/7 мониторинг.";
+  "GuardNOC предоставя премиум управлявани IT услуги за фирми в България - поддръжка, мрежи, cloud, киберсигурност и надежден 24/7 мониторинг.";
 
 export const socials = {
   facebook: "",
